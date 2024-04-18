de43d07f499be05f9a2da94535f49a03b972867a
TasmiaZerin1128 <ttaosnmniya.6638.cross@gmail.com>
2024-04-18 15:16:49 +0600

    Made file name dynamic

--- python/modules/utils/curve_file_generator.py
+++ python/modules/utils/curve_file_generator.py
@@ -9,10 +9,7 @@ class CurveGenerator:
     def __init__(self, data: dict):
         self.data = pd.DataFrame.from_dict(data)
 
-    def createCSV(self, folder_name):
-        if not os.path.exists(f'curvefiles/{folder_name}'):
-            os.makedirs(f'curvefiles/{folder_name}')
-
-        csv_file = f'curvefiles/{folder_name}/first_loader_{datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")}.csv'
+    def createCSV(self, file_name):
+        csv_file = f'curvefiles/{file_name}_{datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")}.csv'
         self.data.to_csv(csv_file, index=False)
         print(f'File successfully created at {csv_file}')
