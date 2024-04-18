930d4721f0b9021360542d55bdd29713fa6eb4d3
TasmiaZerin1128 <ttaosnmniya.6638.cross@gmail.com>
2024-04-18 11:21:29 +0600

    Fixed main branch

--- python/modules/utils/curve_file_generator/__init__.py
+++ python/modules/utils/curve_file_generator/__init__.py
@@ -7,11 +7,3 @@ class CurveGenerator:
 
     def __init__(self, data):
         self.data = data
-
-    def createCSV(self, folder_name):
-        if not os.path.exists(f'curvefiles/{folder_name}'):
-            os.makedirs(f'curvefiles/{folder_name}')
-
-        csv_file = f'curvefiles/{folder_name}/first_loader_{datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")}.csv'
-        self.data.to_csv(csv_file, index=False)
-        print(f'File successfully created at {csv_file}')
