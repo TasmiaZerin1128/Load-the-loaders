fb14f242bba174f4508d4bf717486a6443f74238
TasmiaZerin1128 <ttaosnmniya.6638.cross@gmail.com>
2024-04-19 09:47:08 +0600

    Fixed creating dataframe in pandas

--- python/modules/utils/curve_file_generator.py
+++ python/modules/utils/curve_file_generator.py
@@ -7,7 +7,7 @@ class CurveGenerator:
     data = None
 
     def __init__(self, data: dict):
-        self.data = pd.DataFrame.from_dict(data, orient='index')
+        self.data = pd.DataFrame(data)
 
     def createCSV(self, file_name):
         csv_file = f'curvefiles/{file_name}_{datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")}.csv'
